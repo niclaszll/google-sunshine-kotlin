0cd30105f8a41ec66489e0f1c398af8067a0512b
Niclas <niclaszellerhoff@gmail.com>
2018-01-20 11:50:27 +0100

    removed share option

--- app/src/main/java/com/niicz/sunshinekotlin/detail/DetailFragment.kt
+++ app/src/main/java/com/niicz/sunshinekotlin/detail/DetailFragment.kt
@@ -3,9 +3,9 @@ package com.niicz.sunshinekotlin.detail
 import android.content.Intent
 import android.os.Bundle
 import android.support.v4.app.Fragment
-import android.support.v4.view.MenuItemCompat
-import android.support.v7.widget.ShareActionProvider
-import android.view.*
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.TextView
 import com.niicz.sunshinekotlin.R
 
@@ -13,8 +13,6 @@ import com.niicz.sunshinekotlin.R
 class DetailFragment : Fragment(), DetailContract.View {
 
     override lateinit var presenter: DetailContract.Presenter
-    private val forecastShareHashtag = " #SunshineApp"
-    private var forecastStr: String? = null
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val rootView = inflater!!.inflate(R.layout.fragment_detail, container, false)
@@ -23,35 +21,11 @@ class DetailFragment : Fragment(), DetailContract.View {
 
         val intent = activity.intent
         if (intent != null && intent.hasExtra(Intent.EXTRA_TEXT)) {
-            forecastStr = intent.getStringExtra(Intent.EXTRA_TEXT)
+            val forecastStr = intent.getStringExtra(Intent.EXTRA_TEXT)
             (rootView.findViewById(R.id.detail_text) as TextView).text = forecastStr
         }
 
         return rootView
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
-
-        inflater.inflate(R.menu.detailfragment, menu)
-        val menuItem = menu.findItem(R.id.action_share)
-
-        // Get the provider and hold onto it to set/change the share intent.
-        val mShareActionProvider = MenuItemCompat.getActionProvider(menuItem) as ShareActionProvider
-
-        // Attach an intent to this ShareActionProvider.  You can update this at any time,
-        // like when the user selects a new piece of data they might like to share.
-        mShareActionProvider.setShareIntent(createShareForecastIntent())
-    }
-
-    private fun createShareForecastIntent(): Intent {
-        val shareIntent = Intent(Intent.ACTION_SEND)
-        shareIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET)
-        shareIntent.type = "text/plain"
-        shareIntent.putExtra(
-            Intent.EXTRA_TEXT,
-            forecastStr + forecastShareHashtag
-        )
-        return shareIntent
     }
 
     companion object {
